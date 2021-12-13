63db40f1ba574b2dd368e2e162b73ca1e62f6f52
KUNWAR NIR <92104122+kunwarnir@users.noreply.github.com>
2021-12-13 14:39:17 +0000

    connected the two frames using a button

--- InitialWindow.java
+++ InitialWindow.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.*;
 
 public class InitialWindow extends JFrame implements ActionListener{
 
@@ -17,6 +20,26 @@ public class InitialWindow extends JFrame implements ActionListener{
     label = new JLabel("This is the first frame");
     label.setBounds(150, 30, 300, 220);
     panel.add(label);
+
+    button = new JButton("Start");
+    button.setBounds(260, 350, 80, 50);
+    button.setActionCommand("Start");
+    button.addActionListener(this);
+    panel.add(button);
+
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+
+    switch (e.getActionCommand()){
+      case "Start":
+        BuyWindow myFrame = new BuyWindow();
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myFrame.setSize(600, 500); // set frame size
+        myFrame.setVisible(true); // display frame
+
+    }
 
   }
 
