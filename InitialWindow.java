bdb68d10a74690469e078843b7387ca8ad4fb572
KUNWAR NIR <92104122+kunwarnir@users.noreply.github.com>
2021-12-14 14:59:58 +0000

    edited initial window

--- InitialWindow.java
+++ InitialWindow.java
@@ -6,26 +6,39 @@ import java.awt.*;
 public class InitialWindow extends JFrame implements ActionListener{
 
   private JPanel panel;
-  private JLabel label;
-  private JButton button;
+  private JLabel lblTitle;
+  private JButton btnUser;
+  private JButton btnAdmin;
+
+  private Container base;
+  private CardLayout layout = new CardLayout();
 
   public InitialWindow(){
 
     super("Initial Window");
 
+    base = getContentPane();
+    base.setLayout(layout);
+
     panel = new JPanel();
     panel.setLayout(null);
     add(panel);
 
-    label = new JLabel("This is the first frame");
-    label.setBounds(150, 30, 300, 220);
-    panel.add(label);
+    lblTitle = new JLabel("This is the first frame");
+    lblTitle.setBounds(150, 30, 300, 220);
+    panel.add(lblTitle);
 
-    button = new JButton("Start");
-    button.setBounds(260, 350, 80, 50);
-    button.setActionCommand("Start");
-    button.addActionListener(this);
-    panel.add(button);
+    btnUser = new JButton("User");
+    btnUser.setBounds(260, 350, 80, 50);
+    btnUser.setActionCommand("User");
+    btnUser.addActionListener(this);
+    panel.add(btnUser);
+
+    btnAdmin = new JButton("Admin");
+    btnAdmin.setBounds(100, 350, 80, 50);
+    btnAdmin.setActionCommand("Admin");
+    btnAdmin.addActionListener(this);
+    panel.add(btnAdmin);
 
   }
 
@@ -33,11 +46,17 @@ public class InitialWindow extends JFrame implements ActionListener{
   public void actionPerformed(ActionEvent e) {
 
     switch (e.getActionCommand()){
-      case "Start":
+      case "User":
         BuyWindow myFrame = new BuyWindow();
         myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         myFrame.setSize(600, 500); // set frame size
         myFrame.setVisible(true); // display frame
+        break;
+      case "Admin":
+        BuyWindow other = new BuyWindow();
+        other.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        other.setSize(600, 500); // set frame size
+        other.setVisible(true); // display frame
 
     }
 
