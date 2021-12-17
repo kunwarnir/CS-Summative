fc0996a97728a85730b2bf8520ccd1047ee784cd
KUNWAR NIR <92104122+kunwarnir@users.noreply.github.com>
2021-12-17 14:49:04 +0000

    defined enums

--- Vehicle.java
+++ Vehicle.java
@@ -1,19 +1,23 @@
 public class Vehicle{
   
-  public enum Category{
+  public enum Categories{
     SEDAN, SUV, COUPE, HATCHBACK, TRUCK;
   }
 
-  public enum Colour{
+  public enum Colours{
     SILVER, WHITE, BLUE, BLACK, GRAY, ORANGE, RED, BROWN, GREEN, PINK;
   }
 
-  public enum Type{
+  public enum Types{
     NEW, USED;
   }
 
-  public enum Status{
+  public enum Statuses{
     DEMO, STOCK;
+  }
+
+  public Vehicle(int year, String name, Colours colour, String engine, int cylinders, Categories category, int price, Types type, Statuses status, int age){
+    
   }
   
   public void buy(){
