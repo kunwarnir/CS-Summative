3fbca5827cb4416cc51d06b4d0177619f89f926d
KUNWAR NIR <92104122+kunwarnir@users.noreply.github.com>
2022-01-25 15:58:24 +0000

    added singleton design pattern to user class

--- User.java
+++ User.java
@@ -1,3 +1,12 @@
 public class User extends Person{
+  private static final User INSTANCE = new User();
+
+  private User(){
+
+  }
   
+  public static User getInstance(){
+    return INSTANCE;
+  }
+
 }
