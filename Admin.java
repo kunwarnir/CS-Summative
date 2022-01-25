4663fcb3da2a5cc1367770928fa56a90f33b22dd
KUNWAR NIR <92104122+kunwarnir@users.noreply.github.com>
2022-01-25 15:13:43 +0000

    edited admin class

--- Admin.java
+++ Admin.java
@@ -1,3 +1,16 @@
 public class Admin extends Person {
   
+  public Admin(){
+
+  }
+
+  public void Buy(int balance, int price, Vehicle.Statuses stat){
+    if (price > balance){
+      System.out.println("This car cannot be bought");
+    }
+    else {
+      System.out.println("E");
+    }
+  }
+
 }
